73bc10e6fc4e0a0add7bdba278cc2d79e94a62ab
prtikaher88 <pratikaher88@gmail.com>
2021-01-13 08:45:15 +0530

    Auto-Throttle settings

--- customcrawler/customcrawler/settings.py
+++ customcrawler/customcrawler/settings.py
@@ -36,10 +36,18 @@ ROBOTSTXT_OBEY = False
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 32
 
+# AUTO-THROTTLE SETTINGS
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_DEBUG = True
+AUTOTHROTTLE_START_DELAY = 3
+AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_TARGET_CONCURRENCY = 5
+
+
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
